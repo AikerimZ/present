5288340e4b2971f333882d790787adc1d5cee345
AikerimZ <aigera_0903@mail.ru>
2017-02-03 10:25:53 +0600

    Create Runner.java

---
+++ src/main/java/epam/lab/task1/Runner.java
@@ -0,0 +1,13 @@
+package epam.lab.task1;
+
+/**
+*/
+public class Runner 
+{
+    public static void main( String[] args )
+    {
+        Sweets sweet1 = new Chewy();
+        
+        System.out.println( "Hello World!" );
+    }
+}
